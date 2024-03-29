cc5fec628e8e4ca24529cac33c2cbee29633d19e
Emmanuel Garcia Torres <mendel_77@hotmail.com>
2024-03-29 13:36:19 +0100

    finish implementation with User Schema

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "../../../libs/mongodb";
+import User from "../../../models/user";
+
+export async function GET() {
+	await connectDB();
+
+	const users = await User.find();
+	return NextResponse.json({ code: 200, data: users });
+}
